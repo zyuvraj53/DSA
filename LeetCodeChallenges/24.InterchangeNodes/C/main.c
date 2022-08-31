4278ee091c93bbd9f9df871c8184da3601bfb34b
Yuvraj Singh <zyuvraj53@gmail.com>
2022-08-31 08:52:56 +0530

    leetcode - interchange nodes

--- LeetCodeChallenges/24.InterchangeNodes/C/main.c
+++ LeetCodeChallenges/24.InterchangeNodes/C/main.c
@@ -1,42 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct ListNode {
+//Defining a node
+typedef struct LinkedListNode {
   int val;
-  struct ListNode *next;
-} ListNode;
+  struct LinkedListNode *next;
+} LinkedListNode;
 
-ListNode *insert_at_end(int, ListNode **);
-ListNode *interchange(ListNode *);
-ListNode *change_last(int, ListNode **);
-void print(ListNode *, int);
+void insert_at_end(int, LinkedListNode **);
+LinkedListNode *interchange(LinkedListNode *);
+LinkedListNode *change_last(int, LinkedListNode **);
+void print(LinkedListNode *, int);
 
 int main() {
-  ListNode *LinkedList = NULL;
-  LinkedList = insert_at_end(1, &LinkedList);
-  LinkedList = insert_at_end(2, &LinkedList);
-  //LinkedList = insert_at_end(4, &LinkedList);
-  //LinkedList = insert_at_end(3, &LinkedList);
-  //LinkedList = insert_at_end(7, &LinkedList);
-  //LinkedList = insert_at_end(8, &LinkedList);
+  LinkedListNode *LinkedList = NULL;
+  insert_at_end(1, &LinkedList);
+  insert_at_end(2, &LinkedList);
+  insert_at_end(8, &LinkedList);
   print(LinkedList, 1);
 
-  LinkedList = interchange(LinkedList);
-  print(LinkedList, 2);
+  // LinkedList = interchange(LinkedList);
+  // print(LinkedList, 2);
 
   return 0;
 }
 
-ListNode *interchange(ListNode *head){
-  ListNode *p1 = head;
-  ListNode *p2 = head->next;
+LinkedListNode *interchange(LinkedListNode *head){
+  LinkedListNode *p1 = head;
+  LinkedListNode *p2 = head->next;
 
   if(head == NULL || head->next == NULL)
     return head;
 
   head = p2; 
   while(p1 != NULL || p2 != NULL){
-    ListNode *temp = p2->next;
+    LinkedListNode *temp = p2->next;
     p1->next = temp;
     p2->next = p1;
 
@@ -51,25 +49,25 @@ ListNode *interchange(ListNode *head){
   puts("outside the loop");
 }
 
-ListNode *insert_at_end(int value, ListNode **head) {
-  ListNode *temp = (ListNode *)malloc(sizeof(ListNode));
+void insert_at_end(int value, LinkedListNode **head) {
+  LinkedListNode *temp = (LinkedListNode *)malloc(sizeof(LinkedListNode));
   temp->val = value;
   temp->next = NULL;
   if (*head == NULL) {
     *head = temp;
-    return *head;
+    return;
   }
 
-  ListNode *pointer = *head;
+  LinkedListNode *pointer = *head;
   while (pointer->next != NULL) {
     pointer = pointer->next;
   }
   pointer->next = temp;
-  return *head;
+  return;
 }
 
-void print(ListNode *head, int val) {
-  ListNode *temp = head;
+void print(LinkedListNode *head, int val) {
+  LinkedListNode *temp = head;
   printf("List %d is:\n", val);
   while (temp != NULL) {
     printf("%d →  ", temp->val);
